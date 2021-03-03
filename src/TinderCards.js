8006db6b50fda49ea49ab8acec59f44896bbd900
Samanthaponce5 <samanthaponce5@gmail.com>
2021-03-02 21:06:21 -0500

    backend connected

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,53 +1,22 @@
 import { SwipeableDrawer } from '@material-ui/core'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import TinderCard from "react-tinder-card"
 import './TinderCards.css'
+import axios from './axios'
 
 
 function TinderCards(){
-    const [people, setPeople] = useState([
-        {
-            name: "John",
-            url:'https://i.pinimg.com/originals/90/2a/39/902a392d7af51b9ef526944138d15c98.jpg'
-        },
-        {
-            name: "Aaron",
-            url:'https://i.pinimg.com/originals/ce/0a/9d/ce0a9daae7f226a6cdb263e72fe68174.jpg'
-        },
-        {
-            name: "Marcos",
-            url:'https://i.pinimg.com/originals/97/2e/03/972e0352e034e81d53ce4cd4031ee18c.jpg'
-        },
-        {
-            name: "Sam Kolder",
-            url:'https://i.redd.it/qcjzhnbbh8941.jpg'
-        },
-        {
-            name: "Jenny",
-            url:'https://i1.wp.com/www.andysmarketing.com/wp-content/uploads/2018/01/Paulina-Feb23-2017.jpg?ssl=1'
-        },
-        {
-            name: "Kelsey",
-            url:'https://assets.teenvogue.com/photos/5be4b839368e21796e2d8a9f/1:1/w_3712,h_3712,c_limit/GettyImages-1059211450.jpg'
-        },
-        {
-            name: "Susan",
-            url:'https://images.pexels.com/photos/2811087/pexels-photo-2811087.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-        },
-        {
-            name: "Kelv",
-            url:'https://i1.wp.com/www.menstylefashion.com/wp-content/uploads/2012/12/Godfrey-Gao-in-the-Louis-vuitton.jpg?fit=630%2C350&ssl=1'
-        },
-        {
-            name: "Johnny",
-            url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHOXSepkS39nLqoQz6V8EbKYHqofFOogfeGg&usqp=CAU'
-        },
-        {
-            name: "Jack",
-            url:'https://stanley-models.co.uk/wp-content/uploads/2019/11/Jack-Bull-StanleysModelAgency-BlackMaleModel-Nottingham-UK1-Models.jpg'
+    const [people, setPeople] = useState([])
+
+    useEffect(()=>{
+        async function fetchData(){
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data)
         }
 
-    ])
+        fetchData()
+    },[])
 
     const swiped=(direction, nameToDelete)=>{
         console.log('removing: ' + nameToDelete)
@@ -70,7 +39,7 @@ function TinderCards(){
                 >
 
                     <div
-                    style={{backgroundImage: "url(" + person.url + ")"}}
+                    style={{backgroundImage: "url(" + person.imgUrl + ")"}}
                     className="card"
                     >
                         <h3>{person.name}</h3>
